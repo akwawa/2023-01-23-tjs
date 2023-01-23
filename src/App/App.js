2361851ca9bd6fb238b79f22ecd097aeef192b5f
Akwawa <adm.akwawa@gmail.com>
2023-01-23 14:39:27 +0100

    Ajout du component Button

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './components/ui/Button/Button';
+
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            DEMAT BREIZH
+            <hr/>
+            <Button />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/App/components/ui/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// function Button() {
+/**
+ * simple button
+ * @returns react component structure
+ */
+const Button=()=>{
+    return <button className='Button'>Benjamin</button>;
+}
+
+export default Button;

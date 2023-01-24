636c2e17455f79872d179ee7a6e2b365f2b8c739
Akwawa <adm.akwawa@gmail.com>
2023-01-24 09:51:58 +0100

    Ajout template generate-react-cli

---
+++ src/App/components/ui/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Footer.module.css';
+
+const Footer = (props) => {
+    return <div className={styles.Footer} data-testId="Footer">Footer</div>
+}
+
+export default Footer;
---
+++ src/App/components/ui/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Header.module.css';
+
+const Header = (props) => {
+    return <div className={styles.Header} data-testid="Header">Header</div>
+}
+
+export default Header;
